5d0561409d05a6fdbc195b365ba7b71a9c02b3a9
Carmen Ordoñez <deiord44santiagoov@gmail.com>
2024-02-19 11:41:11 +0100

    recent update

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: deordone <deordone@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 10:22:28 by deordone          #+#    #+#             */
-/*   Updated: 2024/02/17 13:55:04 by deordone         ###   ########.fr       */
+/*   Updated: 2024/02/18 08:01:22 by deordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,20 @@
 # define MINISHELL_H
 # include "../library/libft/libft.h"
 # include "../library/dprintf/ft_dprintf.h"
+
 # define BUG {\
 		dprintf(2, "Ha entrado en -> %s\n", __FILE__);\
 		dprintf(2, "En la funcion -> %s\n", __func__);\
 		dprintf(2, "En la linea -> %d\n", __LINE__);\
 	      }
+
+typedef struct s_analyzer
+{
+	int	input;
+	int	output;
+	int	pipes;
+	char	**cmd;
+	char	*path;
+}	t_analyzer;
+
 #endif
--- src/parser.c
+++ src/parser.c
@@ -6,16 +6,62 @@
 /*   By: deordone <deordone@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 17:15:32 by deordone          #+#    #+#             */
-/*   Updated: 2024/02/17 17:18:31 by deordone         ###   ########.fr       */
+/*   Updated: 2024/02/17 20:03:00 by deordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//vale tienes que hacer muchas cosas 
+//vale tienes que hacer muchas cosas
 //pero sobre todo hacer algo organizado entonces empecemos
 
-< debe redirigir el input
-> debe redirigir el output
+t_token *less_case(t_token *temp, t_analyzer *block)
+{
+	if (!temp->next)
+	       BUG;//personalizar fallo si solo pasamos < y no hay nada despues
+	if (temp->next->type != ARG)
+		BUG;//fallo de strerror
+	temp = temp->next;
+	block->input = open(temp->data, O_RDONLY);
+	if (block->input < 0)
+		BUG; //fallo de strerror
+	temp = temp->next;
+	return (temp);
+}
+//para que entiendas lo que esta pasando por mi cabeza, en teoria tiene que ser una funcion recursiva que me vaya almacenando x cantidadd de estructuras analyzed
+//luego esas estructuras las pasare a mi executor pero aqui tengo que ver que todo este bien hecho
+t_analyzer	*block_parser(t_token *token)
+{
+	t_analyzer block;
+	t_token temp;
 
-token_parser()
+	temp = token;
+	while (temp)
+	{
+		if (token->type == LESS)
+			temp = less_case(temp, &block);
+	//	SI ME PASAN ">"  ENTONCES CREO UN ARCHIVO DEL SIGUIENTE ARGUMENTO SOLAMENTE SI ES DE TIPO ARG
+	//		great_case(token, &block);
+	//	SI ME PASAN "PIPE" ENTONCES QUE EL ARCHIVO SIGUIENTE SEA UN ARG
+	//		pipe_case(token, &block);
+	//	SI ME PASAN "ARG" ENTONCES QUE MIRE EL SIGUIENTE Y SI TIENE "-" o "--" QUE LO GUARDE EN **cmd
+	}
+}
+
+//en el token parser debo separar por bloques de ejecucion, y el final de estos bloques de ejecucion sera un GREAT
+//entonces debo recorrer la lista hasta encontrar un > y en el momento en el que lo encuentre crear la estructura con la tabla de ejecucion
+//cuando tenga esta parte segmentada debo tambien verificar si todo lo que me han pasado es correcto.
+
+void	token_parser(t_token *token)
+{
+	t_token *temp;
+
+	temp = token;
+	while ((temp != GREAT && temp)
+		temp = temp->next;
+	if (temp)
+		printf("index -> %i", ++temp->index);
+	  1			2	3	4	5	6	7
+	[ < ]			[ in ]	[ hola ][ | ]	[ adios ][ > ]	[ out ]
+	block_parser(t_token *token);
+}
