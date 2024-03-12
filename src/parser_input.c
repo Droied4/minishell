060f8c68d34701186209c84cb83912058eb98585
Carmen Ordoñez <deiord44santiagoov@gmail.com>
2024-03-12 17:35:08 +0100

    fixed token interpretation

--- src/parser_input.c
+++ src/parser_input.c
@@ -6,7 +6,7 @@
 /*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 17:15:46 by deordone          #+#    #+#             */
-/*   Updated: 2024/03/12 16:18:35 by deordone         ###   ########.fr       */
+/*   Updated: 2024/03/12 17:34:23 by deordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,13 +94,16 @@ int	input_incomplete(t_shell *sh)
 
 void	new_entry(t_shell *sh)
 {
-	char *new_input;
 	char *new_line;
+	char *new_input;
+	char *last_input;
 
 	new_input = readline(GREEN"\n> "NC);
 	new_line = ft_strdup("\n");
 	new_input = ft_imp_strjoin(new_line, new_input);
-	sh->line = ft_imp_strjoin(sh->line, new_input);
+	last_input = add_space(new_input);
+	free(new_input);
+	sh->line = ft_imp_strjoin(sh->line, last_input);
 	ft_deltoken(&sh->tokens);
 	sh->tokens = generate_tokens(sh->line);
 }
