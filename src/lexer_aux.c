9aed45cfa6750a6eea70b42559e20e15256488cf
Deivid Santiago Ordonez Varela <deordone@cbr12s1.42barcelona.com>
2024-03-14 18:47:33 +0100

    fixed token expansor

--- src/lexer_aux.c
+++ src/lexer_aux.c
@@ -6,7 +6,7 @@
 /*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 18:42:37 by deordone          #+#    #+#             */
-/*   Updated: 2024/03/14 18:11:23 by deordone         ###   ########.fr       */
+/*   Updated: 2024/03/14 18:47:22 by deordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,8 +92,15 @@ static char	*cpy_space(char *final_s, char *s, char btween)
 				if (h >= 2)
 					break ;
 				final_s[j++] = s[i];
-				if (s[i] == s[i + 1] && is_charmeta(s[i + 1]) > 0 && s[i] != '$')
+				if (s[i] == s[i + 1] && is_charmeta(s[i + 1]) > 0 && s[i] != '$') 
 					final_s[j++] = s[++i];
+				else if (s[i] == '$')
+				{
+					while(s[i + 1] != '\0' && s[i] != ' ')
+					{
+						final_s[j++] = s[++i];
+					}
+				}
 			}
 		}
 		else
