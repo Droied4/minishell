f99d7d01b6851d8e4624bdbb6a85949ef3343506
Archly Volcy <avolcy@car12s3.42barcelona.com>
2024-03-25 18:59:22 +0100

    test for ls

--- src/built_echo.c
+++ src/built_echo.c
@@ -6,7 +6,7 @@
 /*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 17:41:07 by avolcy            #+#    #+#             */
-/*   Updated: 2024/03/25 18:00:05 by avolcy           ###   ########.fr       */
+/*   Updated: 2024/03/25 18:25:37 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@
 //echo with -nnnnnnnnnn n n prints : "n nbash", -nnnn nn n n : nn n nbash
 //echo with  -nnnnn- || -nnn-n prints : -nnnnn- \n, -nnn-n \n
 //echo with -nnnn -n
-void    execute_echo(t_shell *shell)
+void    execute_echo(t_shell *sh)
 {
-    (void)shell;
     printf("Hola echo o o o o \n");
+
+    if (sh->tokens->next->data == NULL)
+        printf("\n");
 }
