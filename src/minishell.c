c2004a4ab42e7e542b287ab36f21d362ec01d186
Deivid Santiago Ordonez Varela <deordone@car13s2.42barcelona.com>
2024-03-31 04:19:07 +0200

    comment executor and uncomment builtins

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 10:20:59 by deordone          #+#    #+#             */
-/*   Updated: 2024/03/31 03:41:49 by deordone         ###   ########.fr       */
+/*   Updated: 2024/03/31 04:18:16 by deordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	main(int ac, char **av, char **env)
 		add_history(sh.line);
 		sh.tokens = generate_tokens(sh.line);
 		parse_all(&sh);
-	//	execute_builtins(&sh, env);
+		execute_builtins(&sh, env);
 	//	executor(&sh, env);
 		print_words(sh.words);
 		print_redir(sh.redir);
