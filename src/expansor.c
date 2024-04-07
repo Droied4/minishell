3c60dddce278ad351f7732646a22ab59a52c1d95
Archly <126769273+ZenitsuTHB@users.noreply.github.com>
2024-04-07 21:13:13 +0000

    trying to parser for expansion

--- src/expansor.c
+++ src/expansor.c
@@ -56,7 +56,7 @@ char     *expansion_var(t_shell *sh, char *data, char **env)
     {
         printf("this is %d", i);
         var_node = found_var(data_sp[i], sh->env);
-        if (var_node)
+        if (var_node && is_expandable(data_sp[i]))// cant be there cause split $
         {   
             printf("this is the value--->[%s]\n", var_node->var_content);
             new_data = ft_strdup(var_node->var_content);
@@ -69,18 +69,32 @@ char     *expansion_var(t_shell *sh, char *data, char **env)
     return (new_data);
 }
 
+static int is_expandable(char *data)
+{
+    int len;
+
+    len = ft_strlen(data);
+    if (data[0] == '\"' && data[len] == '\"')
+        return (1);
+    return (0);
+}
+
 void expansor(t_shell *sh, char **env)
 {
-    int i;
+    //int i;
     t_token *tok;
 
-    i = -1;
+    //i = -1;
     tok = sh->tokens;
     while (tok)
     {
-        // if (is_charmeta(tok->data[0]))
-        if (found_dollar(tok->data) == 1)
-           tok->data = expansion_var(sh, tok->data, env);
+        reasign_tok_type(&tok);
+        if (tok->type == 7)
+        {
+            // if (is_charmeta(tok->data[0]))
+            if (found_dollar(tok->data) == 1)
+                tok->data = expansion_var(sh, tok->data, env);
+        }
         tok = tok->next;
     }
 }
