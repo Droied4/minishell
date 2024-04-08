c3de01755b403f311ed93768c7c609ddaf789e0b
archly <starxyz12@gmail.com>
2024-04-08 21:56:36 +0200

    to pull the changes made by Deivid

--- src/expansor.c
+++ src/expansor.c
@@ -6,12 +6,11 @@
 /*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 16:38:28 by avolcy            #+#    #+#             */
-/*   Updated: 2024/04/08 18:45:44 by avolcy           ###   ########.fr       */
+/*   Updated: 2024/04/08 21:54:42 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
 
 static int found_dollar(char *data)
 {
@@ -44,7 +43,7 @@ char     *expansion_var(t_shell *sh, char *data, char **env)
     {
         cpy_data = NULL;
         var_node = found_var(data_sp[i], sh->env);
-        if (var_node && is_expandable(data_sp[i]))// cant be there cause split $
+        if (var_node)//&& is_expandable(data_sp[i]))// cant be there cause split $
         {   
             cpy_data = ft_strdup(var_node->var_content);
             new_data = ft_strjoin2(new_data, cpy_data);
@@ -72,15 +71,15 @@ char     *expansion_var(t_shell *sh, char *data, char **env)
 // 'ho"l"a'
 // ho"l"a
 
-static int is_expandable(char *data)
-{
-    int len;
+// static int is_expandable(char *data)
+// {
+//     int len;
 
-    len = ft_strlen(data);
-    if (data[0] == '\"' && data[len] == '\"')
-        return (1);
-    return (0);
-}
+//     len = ft_strlen(data);
+//     if (data[0] == '\"' && data[len] == '\"')
+//         return (1);
+//     return (0);
+// }
 
 void expansor(t_shell *sh, char **env)
 {
@@ -91,14 +90,14 @@ void expansor(t_shell *sh, char **env)
     tok = sh->tokens;
     while (tok)
     {
-        reasign_tok_type(&tok);
-        if (tok->type == 7)
-        {
+       //reasign_tok_type(&tok);
+        //if (tok->type == 7)
+    //    {
             // if (is_charmeta(tok->data[0]))
             if (found_dollar(tok->data) == 1)
                 tok->data = expansion_var(sh, tok->data, env);
          printf("this is the value--->[%s]\n", tok->data);
-        }
+      //  }
         tok = tok->next;
     }
 }
