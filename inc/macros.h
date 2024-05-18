aca2a57f37e251c51be0d98a27a7fd2d165c9651
ZenitsuTHB <starxyz12@gmail.com>
2024-05-18 11:53:12 +0200

    commit to pull changes made from 42 whithin deivid

--- inc/macros.h
+++ inc/macros.h
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 17:44:17 by deordone          #+#    #+#             */
-/*   Updated: 2024/05/16 11:22:08 by marvin           ###   ########.fr       */
+/*   Updated: 2024/05/16 21:54:41 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,6 @@
 
 # include <signal.h>
 
-extern sig_atomic_t signals;
+extern sig_atomic_t g_signal_var;
 
 #endif
