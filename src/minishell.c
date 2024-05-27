f912902946b5cfe0d54a7a16ead0fa032f7adf89
ZenitsuTHB <starxyz12@gmail.com>
2024-05-27 09:52:38 +0200

    Trying to implement the export variable+=text to concatenate the new value to the old value that was stored in that variable

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 10:20:59 by deordone          #+#    #+#             */
-/*   Updated: 2024/05/26 22:31:04 by avolcy           ###   ########.fr       */
+/*   Updated: 2024/05/27 01:00:16 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ char	*prompt(int exit_status)
 	}
 	return (str);
 }
-
+/*env = simulate_mini_env(sh);*/
 static void	init_sh(t_shell *sh, char **env)
 {
 	sh->exit_status = 0;
@@ -63,13 +63,12 @@ static void	init_sh(t_shell *sh, char **env)
 	sh->pro.r = NULL;
 	if (!*env)
 	{
-		ft_dprintf(2, "\nDON'T do this as the env is NULL\n");
+		ft_dprintf(2, SHELL_ERROR);
 		exit (EXIT_FAILURE);
 	}
-	/*env = simulate_mini_env(sh);*/
 	sh->env = create_lst_env(env);
 }
-//echo $USER""'hello
+/*echo $USER""'hello*/
 int	main(int ac, char **av, char **env)
 {
 	t_shell	sh;
